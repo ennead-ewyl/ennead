f9783c466d9b56427a84a02bbbcd877245358723
VetrivelJaganathan <vetriveljaganathan5125@gmail.com>
2022-05-27 00:01:52 +0530

    study by vetrivel

---
+++ task/26  task/index.js
@@ -0,0 +1,8 @@
+function getValue()
+{
+    username=document.getElementById("user").value
+    password=document.getElementById("pass").value
+     console.log([username,password])
+    document.getElementById("ujkl").innerHTML=username
+
+}
