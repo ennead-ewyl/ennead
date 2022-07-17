f16c99cd21b1f047d562b3fd17ab06bdbb814883
VetrivelJaganathan <vetriveljjaganathan5125@gmail.com>
2022-07-17 21:02:03 +0530

    e-com site off way

---
+++ JavaScript/vetrivel/app.js
@@ -0,0 +1,50 @@
+// varable dicleare
+
+const cartBtn =  document.querySelector('.cart-btn');
+const closeCartBtn =  document.querySelector('.close-cart');
+const clearCartBtn =  document.querySelector('.clear-cart');
+const cartDom =  document.querySelector('.cart');
+const cartOverlay =  document.querySelector('.cart-overlay');
+const  cartItems=  document.querySelector('.cart-items');
+const cartTotal =  document.querySelector('.cart-total');
+const cartContent =  document.querySelector('.cart-content');
+const productsDom =  document.querySelector('.product-content');
+
+// cart 
+let cart = [];
+
+
+// getting product
+class products {
+    async getProducts(){
+        try{
+
+        
+        let result = await fetch('products.json');
+        let data = await result.json();
+        let products = data.items;
+        products = products.map()
+    }catch (error){
+        console.log(error);
+    }
+}
+
+}
+
+// UI
+class UI{}
+
+//local storage
+class storage{
+
+}
+
+
+document.addEventListener("DOMContentLoaded",()=>{
+    const ui = new UI();
+    const products = products();
+
+    // get all pro
+    products.getProducts().then(data => console.log(data));
+
+});
