ee221dff857303474c8080afe17ff07123d78f65
kavinkumar <kavinkumars218@gmail.com>
2022-07-18 14:45:19 +0530

    modified javascript

--- kavin-js/character/index.js
+++ kavin-js/character/index.js
@@ -8,45 +8,9 @@ function _fetchData() {
               console.log(item);
              
               let ele1 = document.querySelector(".content");
-              let breakEle = document.createElement("br");
-  
               let img = document.createElement("img");
               img.setAttribute("src",item.img)
-              img.style.width="150px";
-              img.style.height="200px";
-             img.style.marginLeft="30px"
               ele1.append(img);
-              ele1.append(breakEle);
-  
-              let ele2 = document.createElement("label");
-              ele2.setAttribute("for", item);
-              ele2.textContent = item.name;  
-              ele1.append(ele2);
-              ele1.append(breakEle);
-            
-              let nickname = document.createElement("label");
-              nickname.setAttribute("for", item);
-              nickname.textContent = `Nickname:${item.nickname}`; 
-              ele1.append(nickname);
-              ele1.append(breakEle);
-              
-              let occupation = document.createElement("label");
-              occupation.setAttribute("for", item);
-              occupation.textContent = `Occupation:${item.occupation}`;
-              ele1.append(occupation);
-              ele1.append(breakEle);
-  
-              let potrayed = document.createElement("label");
-              potrayed.setAttribute("for", item);
-              potrayed.textContent = `Potrayed:${item.potrayed}`;
-              ele1.append(potrayed);
-              ele1.append(breakEle);
-  
-              let status = document.createElement("label");
-              status.setAttribute("for", item);
-              status.textContent = `Status:${item.status}`;  
-              ele1.append(breakEle);
-  
           });
         })
       }
