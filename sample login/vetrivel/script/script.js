0229ff5238c8d27d0a56ebd5d6d83ed437d47d8a
VetrivelJaganathan <vetriveljjaganathan5125@gmail.com>
2022-07-20 20:33:10 +0530

    sample form website file insert

---
+++ sample login/vetrivel/script/script.js
@@ -0,0 +1,9 @@
+function getInfo(event){
+    event.preventDefault();
+    let userName = document.querySelector('.userName').value;
+    let department = document.querySelector('.department').value;
+    let place = document.querySelector('.place').value;
+    console.log(userName);
+}
+
+getInfo();
