c5f60ef44bc52d5de684fbc70d41c4cd9c9009b9
VetrivelJaganathan <vetriveljjaganathan5125@gmail.com>
2022-07-20 22:44:06 +0530

    sample login submit value print in website

--- sample login/vetrivel/script/script.js
+++ sample login/vetrivel/script/script.js
@@ -1,9 +1,25 @@
-function getInfo(event){
-    event.preventDefault();
-    let userName = document.querySelector('.userName').value;
-    let department = document.querySelector('.department').value;
-    let place = document.querySelector('.place').value;
-    console.log(userName);
+function details(totalInfo){
+    let Local = localStorage.setItem("strlens", JSON.stringify(totalInfo));
+    document.querySelector('.userName').value='';
+    document.querySelector('.Department').value='';
+    document.querySelector('.place').value='';
+    
 }
 
-getInfo();
+function getInfo(event) {
+    event.preventDefault();
+    let userName = document.querySelector('.userName').value;
+    let department = document.querySelector('.Department').value;
+    let place = document.querySelector('.place').value;
+    let result = document.querySelector('.result');
+    details({userName,department,place});
+    result.innerHTML = `<span> ${userName} </span><br>
+    <span> ${department} </span><br>
+    <span> ${place} </span><br>`
+
+    
+}
+
+
+
+
