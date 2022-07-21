b27000b8a016f9ac86644e0b473a97bfc8375b34
VetrivelJaganathan <vetriveljjaganathan5125@gmail.com>
2022-07-21 17:39:26 +0530

    sample loggin file changed

--- sample login/vetrivel/script/script.js
+++ sample login/vetrivel/script/script.js
@@ -1,5 +1,5 @@
 function details(totalInfo){
-    let Local = localStorage.setItem("strlens", JSON.stringify(totalInfo));
+    // let Local = localStorage.setItem("strlens", JSON.stringify(totalInfo));
     document.querySelector('.userName').value='';
     document.querySelector('.Department').value='';
     document.querySelector('.place').value='';
@@ -8,14 +8,28 @@ function details(totalInfo){
 
 function getInfo(event) {
     event.preventDefault();
-    let userName = document.querySelector('.userName').value;
-    let department = document.querySelector('.Department').value;
+    let userName = document.querySelector('.userName').value  ;
+    let department =document.querySelector('.Department').value ;
     let place = document.querySelector('.place').value;
     let result = document.querySelector('.result');
+    if(localStorage.getItem('Data')== null){
+        localStorage.setItem('Data', '[]');
+    }
+    let lastData =JSON.parse(localStorage.getItem('Data'));
+    // console.log(lastData);
+    lastData.push(userName,department,place);
+    localStorage.setItem('Data',JSON.stringify(lastData));
     details({userName,department,place});
-    result.innerHTML = `<span> ${userName} </span><br>
-    <span> ${department} </span><br>
-    <span> ${place} </span><br>`
+    if(localStorage.getItem('Data')!=null){
+        result.innerHTML= JSON.parse(localStorage.getItem('Data'));
+    }
+
+    // let newData = JSON.parse(lastData);
+    // console.log(newData);
+    
+    // result.innerHTML = `<span> ${userName} </span><br>
+    // <span> ${department} </span><br>
+    // <span> ${place} </span><br>`
 
     
 }
